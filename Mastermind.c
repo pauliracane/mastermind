45ea01351b0fd92e73938f82de421e9c64f38f21
Paul Iracane <piracane@localhost.localdomain>
2015-11-05 15:33:57 -0500

    Commented failed attempt at self-playing function.

--- Mastermind.c
+++ Mastermind.c
@@ -131,34 +131,43 @@ int whitecheck(char *changingNum, char *input)
 
 int playself(char *changingNum)
 {
+	//set asside storage for ints and arrays and what have you
 	int y = 0, guesses = 0,red = 0;
 	char answer[5]={'x', 'x', 'x', 'x', '\0'};
 	char guess[5] = {'0','0','0','0','\0'};
 	char values[5] = {'x', 'x', 'x', 'x', '\0'};
 	char wrong = '\0';
+	//for each number
 	for (int x = 0; x < 10; x++)
 		{
+		//Guess = 4 of that number
 		for (int z = 0; z < 4; z++)
 			guess[z]=guess[z]+x;
+		
 		red = redcheck(changingNum,guess);
 		guesses++;
-		
+		//If guess = answer, break, break, break.
 		if (red == 4)
 			{
 			strncpy(answer, guess, sizeof(answer));
 			break;
 			}
+		//Else, store that number into hte next 'y' spots of 'values'
 		for (y=y;y<(y+red);y++)
 			values[y]=x+'0';
+		//if there is no reds, store that value at "wrong"
 		if (!red)
 			wrong = x + '0';
 		}
+	//Set all digits in guess to wrong
 	for (int x = 0; x<4; x++)
 		guess[x]=wrong;
+	//Go through each value stored at Values in each location guess
 	for (int x = 0; x<4; x++){
 		for (int y = 0; y < 4; y ++){
 			guess[x]=values[y];
 			guesses++;
+			//Check for reds in RXXX, then repeat on 2nd value.
 			if(redcheck(changingNum,guess))
 				{
 				answer[x]=values[y];
